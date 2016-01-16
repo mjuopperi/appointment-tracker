0fa2c5fae6ae7ae1fc45ea7b0fbede95d41ccb6b
Markus Juopperi <markus.juopperi@kolumbus.fi>
2016-01-16 13:42:09 +0200

    Quit app when window is closed

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-var app = require('app')
-var BrowserWindow = require('browser-window')
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
 
 app.on('ready', function() {
   var mainWindow = new BrowserWindow({
     width: 1000,
     height: 600,
     minWidth: 1000
-  })
+  });
   mainWindow.loadURL('file://' + __dirname + '/main.html')
-})
+});
